53405c1a1becf7ebce06b5c77f209957a97b5a38
btnwilson <138797732+btnwilson@users.noreply.github.com>
2023-10-12 19:04:18 -0400

    Update parsingError.py

--- parsingError.py
+++ parsingError.py
@@ -1,4 +1,4 @@
-print("Hello, World!")
+import os 
 def load_report(filename):
     file = open(filename, 'r')
     error_modes = {}
@@ -6,6 +6,9 @@ def load_report(filename):
         if ':' in line and "AUTOMATED REPORT PERIOD" not in line and "OPERATION NOTES" not in line:
             segments = line.split(':')
             error_modes[segments[0].strip()] = int(segments[1].strip())
+    file.close()
     return error_modes      
-    
-error_modes = load_report("reports/reports/000000.dat")  
+
+data_centers = {}
+for file in os.listdir('reports/'):
+    data_centers[file.strip('.dat')] = load_report("reports/000000.dat") 
