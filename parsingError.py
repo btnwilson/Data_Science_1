ce3bf469cab8b1894430ea5af6c8d5d42a344d74
btnwilson <138797732+btnwilson@users.noreply.github.com>
2023-10-18 00:53:10 -0400

    Update parsingError.py

--- parsingError.py
+++ parsingError.py
@@ -11,17 +11,19 @@ def load_report(filename):
 
 data_centers = {}
 for file in os.listdir('reports/'):
-    data_centers[file.strip('.dat')] = load_report("reports/000000.dat") 
-print('Files in Reports Directory:'.format(), len(os.listdir('reports/')))
-print('Size of the Data Centers Dictionary:'.format(), len(data_centers))
+    data_centers[file.strip('.dat')] = load_report('reports/' + file)  
+# %%
 
-data_center_error_avgs = {}
+data_center_total_error = {}
 total_errors = 0
+abnormal_tag = []
 for key in data_centers.keys():
+    if len(key) != 6 or not key.isdigit():
+        abnormal_tag.append(key)
     errors = sum(data_centers[key].values())
-    data_center_error_avgs[key] = errors
+    data_center_total_error[key] = errors
     total_errors += errors
-
+most_errors = sorted(data_center_total_error.items(), key=lambda error: error[1], reverse=True)
 error_type_totals = {}
 for key in data_centers.keys():
     for error in data_centers[key].keys():
@@ -29,5 +31,13 @@ for key in data_centers.keys():
             error_type_totals[error] = data_centers[key][error]
         else:
             error_type_totals[error] += data_centers[key][error]
-top_2 = sorted(error_type_totals.items(), key=lambda error: error[1], reverse=True)[:2]
-print(top_2)
+
+
+top_10_errors = sorted(error_type_totals.items(), key=lambda error: error[1], reverse=True)
+print('Files in Reports Directory:'.format(), len(os.listdir('reports/')))
+print('Size of the Data Centers Dictionary:'.format(), len(data_centers))
+# %%
+flood_values = {}
+for key in data_centers.keys():
+    flood_values[key] = data_centers[key]['Physical intrusion (water)']
+most_floods = sorted(flood_values.items(), key=lambda flood: flood[1], reverse=True)
